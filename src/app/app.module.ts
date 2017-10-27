a8cea35f88e803809daad9cca1befeb31c694a2f
beachside-project <brave.mont.saint.micheal@gmail.com>
2017-10-27 15:26:10 +0900

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,18 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule } from '@angular/common/http';
 import { AppComponent } from './app.component';
 import { BrowserModule } from '@angular/platform-browser';
+import { MaterialModule } from './material.module';
+
 
 @NgModule({
   imports: [
-  BrowserModule],
+    BrowserModule,
+    FormsModule,
+    HttpClientModule,
+    MaterialModule,
+  ],
   declarations: [AppComponent],
   bootstrap: [AppComponent]
 })
